e7c8779f54d33e58c3d5c2986e01a8cf76601e87
ManuelAOValle <manuelalberto.ortizvalle@gmail.com>
2017-04-28 00:00:51 -0500

    Actualización Readme

--- Optica/Proyecto_optica_Metamaterial.m
+++ Optica/Proyecto_optica_Metamaterial.m
@@ -1,27 +1,27 @@
 clear;close all
-%Se define longitud de lado de regi?n 1 (con metamaterial ZIM)
+%Se define longitud de lado de region 1 (con metamaterial ZIM)
 a=4.5e-6;
 c=3e8;%Se defien
-%De acuerdo al art?culo, se define un valor para la frecuencia de
+%De acuerdo al articulo, se define un valor para la frecuencia de
 %resonancia para la permitividad y la permeabilidad del material
 wp=2*pi*(c/a);
 %Se define el factor de amortiguamiento a partir de la frecuencia de
-%resonancia (se elige peque?o de acuerdo al art?culo)
+%resonancia (se elige peque?o de acuerdo al articulo)
 gamma=(1e-25)*wp;
 w=linspace(0.8*wp,1.2*wp,1000);%Se define vector de frecuencias
 %Se definen el valor de la permitividad relativa en la regi?n con el ZIM
 e1=1-(wp^2)./(w.*(w+i*gamma*w))
-%Se definen el ?rea y la altura de la regi?n 1, as? como la longitud de
-%onda de la luz en tal regi?n, la cual debe ser grande para obtener el
-%efecto de superacoplamiento y no sufrir desfase en la transmisi?n
+%Se definen el area y la altura de la region 1, asi como la longitud de
+%onda de la luz en tal region, la cual debe ser grande para obtener el
+%efecto de superacoplamiento y no sufrir desfase en la transmision
 A=a^2;
 d=a;
 lambda=0.8*a;
-%Se define la magnitud del vector de propagaci?n
+%Se define la magnitud del vector de propagacion
 ko=(2*pi)/(lambda);
-%Se definen en base al modelo de Drude y el art?culo, los coeficientes de
-%relexi?n y transmisi?n de Fresnel para el caso en la frontera de la regi?n
-%1 y el conductor magn?tico perfecto
+%Se definen en base al modelo de Drude y el articulo, los coeficientes de
+%relexion y transmision de Fresnel para el caso en la frontera de la region
+%1 y el conductor magnetico perfecto
 R=-(i*ko*e1*A)./(2*d+(i*ko*e1*A));
 T=1+R;
 wnorm=w/wp;%De define un vector de frecuencias normalizado
